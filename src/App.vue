95e81f756095f5a86b93ae5117ddf9d340735e2b
Rafael Menezes <menezes.carloeng@gmail.com>
2022-02-23 19:25:39 -0300

    start project, api integration

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="corpo">
+        <h1 class="centralizado">{{ titulo }}</h1>
+
+        <ul class="lista-fotos">
+            <li class="lista-fotos-item" v-for="foto of fotos" :key="foto.titulo">
+                <img :src="foto.url" :alt="foto.titulo">
+            </li>
+        </ul>
+
+
+    </div>
+</template>
+
+<script>
+export default {
+
+    data() {
+        return {
+            titulo: 'Arrombadineos',
+            fotos: []
+        }
+    }, 
+        
+created() {
+    this.$http.get('http://localhost:3000/v1/fotos')
+        .then(res=> res.json())
+        .then(fotos => this.fotos = fotos, err => console.log(err));
+    }
+
+}
+</script>
+
+<style>
+
+</style>
