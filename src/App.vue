741b0394cad2d20bf4786e55fc08eadea00ddd6e
Rafael Menezes <menezes.carloeng@gmail.com>
2022-03-05 11:45:16 -0300

    generating routes

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,11 @@
 <template>
   <div class="corpo">
-    <h1 class="centralizado">{{ titulo }}</h1>
-    <input class="filtro" @input="filtro = $event.target.value" placeholder="Pesquise a foto/imagem">
-        <ul class="lista-fotos">
-            <li class="lista-fotos-item" v-for="foto in fotosFiltro" :key="foto.titulo">
-                <meu-painel :titulo="foto.titulo">
-                    <imagem-responsiva :src="foto.url" :alt="foto.titulo" />
-                </meu-painel>
-            </li>
-        </ul>
+  <router-view></router-view>
   </div>
 </template>
 
 <script>
-import ImagemResonsiva from './components/shared/painel/imagem-responsiva/ImagemResonsiva.vue';
-import Painel from './components/shared/painel/Painel.vue';
 
-export default {
-
-    components: {
-
-        'meu-painel': Painel,
-        'imagem-responsiva': ImagemResonsiva
-    },
-
-    data () {
-        return {
-
-        titulo: 'IMG IMG', 
-        fotos: [],
-        filtro: ''
-        }
-    },
-        
-    computed:{
-        fotosFiltro(){
-            if(this.filtro){
-                let exp = new RegExp(this.filtro.trim(), 'i');
-                return this.fotos.filter(foto => exp.test(foto.titulo));
-            }else{
-                return this.fotos;
-            }
-        }
-    },
-
-    created() {
-
-        this.$http
-        .get('http://localhost:3000/v1/fotos')
-        .then(res => res.json())
-        .then(fotos => this.fotos = fotos, err => console.log(err));
-    }
-}
 </script>
 
 <style>
@@ -60,23 +14,5 @@ export default {
     width: 98%;
     margin: 0 auto;
 }
-
-.centralizado {
-    text-align: center;
-}
-
-.lista-fotos {
-    list-style: none;
-}
-
-.lista-fotos .lista-fotos-item {
-    display: inline-block;
-}
-
-.filtro {
-    display: block;
-    width: 100%;
-}
-
 
 </style>
